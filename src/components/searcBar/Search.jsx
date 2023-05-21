56ece269aa88f6b132f4be69d07c4a8e0042baa5
Alejandro Yunes <alejandro.yunes@inxeption.com>
2023-05-21 16:30:18 -0500

    made search box responsive

--- src/components/searcBar/Search.jsx
+++ src/components/searcBar/Search.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import './search.scss'
 import Logo from '../../resources/logo.png'
-import Magnet from '../../resources/search.png'
+import SearchIcon from '../../resources/search.png'
 
 const SearchBar = () => (
-  <div className='search-wrapper'>
+  <>
+    <section className='search-wrapper'>
+      <div className='search-content'>
 
-    <div className='logo'>
-      <img src={Logo} alt='Logo Mercado Libre' />
-    </div>
-    <div className='search-product__input'>
-      <div className='search-input'>
-        <input type='text' placeholder='Nunca dejes de buscar' />
-      </div>
-      <div className='search-magnet'>
-        <img src={Magnet} alt='buscar buton Mercado Libre' />
-      </div>
-    </div>
+        <div className='logo'>
+          <img src={Logo} alt='Logo Mercado Libre' />
+        </div>
 
-  </div>
+        <div className='search-product__input'>
+          <div className='search-input'>
+            <input type='text' placeholder='Nunca dejes de buscar' />
+          </div>
+          <div className='search-icon'>
+            <img src={SearchIcon} alt='buscar buton Mercado Libre' />
+          </div>
+        </div>
+        
+      </div>
+    </section>
+  </>
 )
 
 export default SearchBar
